75e69625aea6abe29fb91b59ef11caf9352f32dd
Fatin Haque <frhaque@gmail.com>
2017-12-01 17:24:34 -0500

    Seperating out presentational and container components, Angular Fundamentals

---
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core/src/metadata/directives';
+import { Passenger } from '../../models/passenger.interface';
+
+@Component({
+    selector: 'passenger-count',
+    template: `
+        <div>
+            <h3>Airline Passengers</h3>
+            <div>
+                Passengers Checked In: {{ checkedInCount() }} / {{ items.length }};
+            </div>
+        </div>
+    `
+})
+export class PassengerCountComponent {
+    @Input() items: Passenger[];
+
+    checkedInCount(): number {
+        if (!this.items) return;
+        return this.items.filter( (item: Passenger) => item.checkedIn ).length;
+    }
+}
---
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -0,0 +1,70 @@
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { Passenger } from '../../models/passenger.interface';
+
+@Component({
+    selector: 'passenger-detail',
+    styleUrls: ['passenger-detail.component.scss'],
+    template: `
+        <div>
+            <span 
+            class="status"
+            [class.checked-in]="detail.checkedIn"></span>
+            
+            <div *ngIf="editing">
+                <input 
+                    #name
+                    type="text" 
+                    [value]="detail.fullname"
+                    (input)="onNameChange(name.value)">
+            </div>
+            <div *ngIf="!editing">{{ detail.fullname }}</div>
+            <div class="date">
+            Check in date: 
+            {{ 
+                detail.checkInDate ? 
+                (detail.checkInDate | date: 'yMMMMd' | uppercase) 
+                : 
+                'Not checked in' 
+            }}
+            </div>
+            <div class="children">
+            Children: {{ detail.children?.length || 0 }}
+            </div>
+            <button type="button" (click)="toggleEditing()">
+                {{editing ? 'Done' : 'Edit'}}
+            </button>
+            <button type="button" (click)="onRemove()">
+                Remove
+            </button>
+        </div>
+    `
+})
+export class PassengerDetailComponent implements OnChanges {
+    @Input()    detail: Passenger;
+    @Output()   remove: EventEmitter<any> = new EventEmitter<any>();
+    @Output()   edit: EventEmitter<any> = new EventEmitter<any>();
+
+    editing: boolean = false;
+
+    ngOnChanges(changes) {
+        if (changes.detail) {
+            this.detail = {...changes.detail.currentValue as Passenger};
+        }
+    }
+
+    onNameChange(name: string) {
+        this.detail.fullname = name;
+    }
+
+    toggleEditing() {
+        if (this.editing) {
+            this.edit.emit(this.detail);
+        }
+
+        this.editing = !this.editing;
+    }
+
+    onRemove() {
+        this.remove.emit(this.detail);
+    }
+}
